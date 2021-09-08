644f83e171103b1048cae428c7568c0bea2905c3
dedechandran <dede.nugraha@accenture.com>
2021-09-08 22:54:37 +0700

    Added leak canary

--- app/src/main/java/com/dedechandran/movieapps/ui/MainActivity.kt
+++ app/src/main/java/com/dedechandran/movieapps/ui/MainActivity.kt
@@ -12,15 +12,31 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val vm by viewModels<MainViewModel>()
-    private lateinit var binding: ActivityMainBinding
+    private var binding: ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(binding?.root)
         vm.connectionState.observe(this) { isHasConnectivity ->
-            if(isHasConnectivity) return@observe
-            Snackbar.make(binding.root,resources.getString(R.string.no_network_connection_message),Snackbar.LENGTH_SHORT).show()
+            if (isHasConnectivity) return@observe
+            binding?.root?.let {
+                Snackbar.make(
+                    it,
+                    resources.getString(R.string.no_network_connection_message),
+                    Snackbar.LENGTH_SHORT
+                ).show()
+            }
+
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+
+    override fun onBackPressed() {
+        finishAfterTransition()
     }
 }
